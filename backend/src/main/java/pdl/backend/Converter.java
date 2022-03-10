07e1a4e7da9fe14ab207434404ba3fa0bd8fab3e
Thomas Clement <thomas.clement@etu.u-bordeaux.fr>
2022-03-10 16:30:49 +0100

    added image conversion between backend and process

---
+++ backend/src/main/java/pdl/backend/Converter.java
@@ -0,0 +1,40 @@
+package pdl.backend;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+
+import boofcv.io.image.ConvertBufferedImage;
+import boofcv.struct.image.GrayU8;
+import boofcv.struct.image.Planar;
+
+public class Converter {
+    public static Planar<GrayU8> InputStreamToPlanar(InputStream input) {
+        try {
+            BufferedImage buffer = ImageIO.read(input);
+            // Planar<GrayU8> planar = ConvertRaster.bufferedToPlanar_U8(buffer, dst);
+            Planar<GrayU8> planar = ConvertBufferedImage.convertFromPlanar(buffer, null, true, GrayU8.class);
+            return planar;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static InputStream PlanarToInputStream(Planar<GrayU8> input) {
+        try {
+            BufferedImage buffer = ConvertBufferedImage.convertTo_U8(input, null, true);
+            ByteArrayOutputStream bytearray = new ByteArrayOutputStream();
+            ImageIO.write(buffer, "jpeg", bytearray);
+            InputStream output = new ByteArrayInputStream(bytearray.toByteArray());
+            return output;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
