5a3d022739adb81cb34a150737a55f86bfbf4181
Ismail El aissate <ismail.el-aissate@etu.u-bordeaux.fr>
2022-03-17 14:10:24 +0100

    chge 9

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -17,15 +17,13 @@ api.getImageList()
     console.log(e.message);
   });
 
-  function debugBase64(base64URL){
+  function windowframe64(base64URL){
     var win = window.open();
     win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
 }
 
   function select(e){
-  alert(e.target.id);
   var modal = document.getElementById("myModal");
-
   var modalImg = document.getElementsByClassName("modal-content")
   var captionText = document.getElementById("caption");
   const elem = e.target;
@@ -33,7 +31,7 @@ api.getImageList()
   modalImg.src = elem.dataset.biggerSrc || elem.src;
   captionText.innerHTML = elem.alt;
   window.location.href = modalImg.src;
-  debugBase64(modalImg.src);
+  windowframe64(modalImg.src);
   //router.push({ name: 'image', params: { id: selectedId.value } })
 
   }
