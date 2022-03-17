1e70b9493f592a4fd5ec570147454a3b378af265
Ismail El aissate <ismail.el-aissate@etu.u-bordeaux.fr>
2022-03-17 14:06:36 +0100

    Change 9

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { api } from '@/http-api';
-import { ImageType } from '@/image'
+import { ImageType } from '@/image';
+import router from "@/router";
 import ImageDisplay from './ImageDisplay.vue';
+import { Component } from '@angular/core';
 
 const imageList = ref<ImageType[]>([]);
+var selectedId = ref(-1);
 
 api.getImageList()
   .then((data) => {
@@ -13,14 +16,68 @@ api.getImageList()
   .catch(e => {
     console.log(e.message);
   });
+
+  function debugBase64(base64URL){
+    var win = window.open();
+    win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+}
+
+  function select(e){
+  alert(e.target.id);
+  var modal = document.getElementById("myModal");
+
+  var modalImg = document.getElementsByClassName("modal-content")
+  var captionText = document.getElementById("caption");
+  const elem = e.target;
+  modal.style.display = "block";
+  modalImg.src = elem.dataset.biggerSrc || elem.src;
+  captionText.innerHTML = elem.alt;
+  window.location.href = modalImg.src;
+  debugBase64(modalImg.src);
+  //router.push({ name: 'image', params: { id: selectedId.value } })
+
+  }
+
+
 </script>
 
+
+
+
 <template>
+<h3>Gallery</h3>
   <div>
-    <h3>Gallery</h3>
-    <ImageDisplay v-for="image in imageList" :id="image.id" />
-  </div>
+  <div id="myModal" class="modal">
+    <ImageDisplay class="modal-content" v-for="image in imageList" :id="image.id" @click="select($event)" />
+  </div></div>
+   <div id="caption"></div>
 </template>
 
-<style scoped>
+
+
+
+
+
+
+<style>
+
+#gallery{
+    display: inline-flex;
+    flex-flow: row;
+    padding: 10px;
+    margin-left: auto;
+    margin-right: auto;
+}
+img {
+    max-width: 220px;
+    max-height: 220px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.3s;
+}
+img:hover
+{
+   opacity: 0.7;
+
+}
 </style>
