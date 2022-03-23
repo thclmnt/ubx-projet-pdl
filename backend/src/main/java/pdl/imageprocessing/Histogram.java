72bce99a95fdcd051a1d0b8ba399ed150a40f214
adihia <achrafdi@icloud.com>
2022-03-23 23:25:34 +0100

    add histo and outline

---
+++ backend/src/main/java/pdl/imageprocessing/Histogram.java
@@ -0,0 +1,76 @@
+package main.java.pdl.imageprocessing;
+
+import boofcv.io.image.UtilImageIO;
+import boofcv.struct.image.GrayU8;
+
+public class Histogram {
+
+    private int min;
+    private int max;
+    public GrayU8 input;
+    private int [] calH;
+    private int [] cumH;
+
+    public Histogram(GrayU8 input){
+        this.input = input;
+        calH = new int[256];
+        for(int ng=0; ng<255; ng++)
+			calH[ng]= 0;
+        cumH = new int[256];
+        for(int ng=0; ng<255; ng++)
+            cumH[ng]= 0;
+    }
+
+    public int retrieveMax(){
+        this.max = this.input.get(0, 0);
+        for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				if(this.input.get(x, y) > this.max){
+                    this.max = this.input.get(x, y);
+                }
+			}
+		}
+        return this.max;
+    }
+
+    public int retrieveMin(){
+        this.min = this.input.get(0, 0);
+        for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				if(this.input.get(x, y) < this.min){
+                    this.min = this.input.get(x, y);
+                }
+			}
+		}
+        return this.min;
+    }
+
+    public int [] calculHisto(){
+        for (int y = 0; y < input.height; ++y) {
+		    for (int x = 0; x < input.width; ++x) {
+                this.calH[this.input.get(x, y)] ++;
+            }
+        }
+        return this.calH;
+    }
+
+    public int [] histoCum(){
+        this.calH = calculHisto();
+        int tmp = 0;
+        for(int i=0; i<cumH.length; i++){
+            this.cumH[i] = tmp + this.calH[i];
+            tmp = cumH[i];
+        }
+        return this.cumH;
+    }
+
+    public boolean compare(int [] imgComp){
+        boolean comp= true; 
+        for (int i=0; i<calH.length; i++){
+            if(calH[i]!=imgComp[i])
+                comp=false;
+        }
+        return comp;
+    }
+    
+}
--- backend/src/main/java/pdl/imageprocessing/Outline.java
+++ backend/src/main/java/pdl/imageprocessing/Outline.java
@@ -1,5 +1,79 @@
 package pdl.imageprocessing;
 
+import javax.print.attribute.standard.MediaSize.Other;
+import boofcv.alg.filter.blur.GBlurImageOps;
+import boofcv.alg.feature.detect.extract.NonMaxBlockSearchRelaxed.Max;
+import boofcv.alg.filter.convolve.GConvolveImageOps;
+import boofcv.io.image.ConvertBufferedImage;
+import boofcv.io.image.UtilImageIO;
+import boofcv.struct.convolve.Kernel1D_S32;
+import boofcv.struct.convolve.Kernel2D_S32;
+import boofcv.struct.image.GrayU8;
+import boofcv.struct.image.Planar;
+
+import java.awt.image.BufferedImage;
+
+
+
 public class Outline {
+    public static void outline(Planar<GrayU8> image, Planar<GrayU8> output){
+        int [][]sobel_x = {{-1,0,1},
+                          {-2,0,2},
+                          {-1,0,1}};
     
+        int [][] sobel_y = {{-1,-2,-1},
+                            {0,0,0},
+                             {1,2,1}};
+    
+        for(int y=sobel_x.length/2; y<image.height-sobel_x.length/2; ++y){
+          for( int x=sobel_x.length/2; x<image.width-sobel_x.length/2; ++x){
+            int pixel_x0 = 0;
+            int pixel_x1 = 0;
+            int pixel_x2 = 0;
+
+            int pixel_y0 = 0;
+            int pixel_y1 = 0;
+            int pixel_y2 = 0;
+
+            int val0 = 0;
+            int val1 = 0;
+            int val2 = 0;
+
+            for(int u = -sobel_x.length/2; u <= sobel_x.length/2; u++){
+              for(int v = -sobel_x.length/2; v <= sobel_x.length/2; v++){
+
+                pixel_x0 = pixel_x0 + (image.getBand(0).get(x + u, y + v) * sobel_x[u + (sobel_x.length/2)][v + (sobel_x.length/2)]);
+                pixel_x1 = pixel_x1 + (image.getBand(1).get(x + u, y + v) * sobel_x[u + (sobel_x.length/2)][v + (sobel_x.length/2)]);
+                pixel_x2 = pixel_x2 + (image.getBand(2).get(x + u, y + v) * sobel_x[u + (sobel_x.length/2)][v + (sobel_x.length/2)]);
+
+                pixel_y0 = pixel_y0 + (image.getBand(0).get(x + u, y + v) * sobel_y[u + (sobel_y.length/2)][v + (sobel_y.length/2)]);
+                pixel_y1 = pixel_y1 + (image.getBand(1).get(x + u, y + v) * sobel_y[u + (sobel_y.length/2)][v + (sobel_y.length/2)]);
+                pixel_y2 = pixel_y2 + (image.getBand(2).get(x + u, y + v) * sobel_y[u + (sobel_y.length/2)][v + (sobel_y.length/2)]);
+
+                
+                val0 = (int) Math.sqrt((pixel_x0 * pixel_x0) + (pixel_y0 * pixel_y0));
+                val1 = (int) Math.sqrt((pixel_x1 * pixel_x1) + (pixel_y1 * pixel_y1));
+                val2 = (int) Math.sqrt((pixel_x2 * pixel_x2) + (pixel_y2 * pixel_y2));
+
+                if(val0 < 0) 
+                    val0 = 0;
+                if(val0 > 255) 
+                    val0 = 255;
+                if(val1 < 0) 
+                    val1 = 0;
+                if(val1 > 255) 
+                    val1 = 255;
+                if(val2 < 0) 
+                    val2 = 0;
+                if(val2 > 255) 
+                    val2 = 255;
+              }
+            }
+            output.getBand(0).set(x, y, val0);
+            output.getBand(1).set(x, y, val1);
+            output.getBand(2).set(x, y, val2);
+
+          }
+        }
+    }
 }
